10ce30f6c35025012098b104f9f10327be7ff1c2
Sapna Jha <sapnajha1919@gmail.com>
2023-04-20 19:36:59 +0530

    Set up Firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDcZfnd2207rFbt5x5fVm_D4ra80kos2Qo",
+  authDomain: "music-app-7d786.firebaseapp.com",
+  projectId: "music-app-7d786",
+  storageBucket: "music-app-7d786.appspot.com",
+  messagingSenderId: "843654201707",
+  appId: "1:843654201707:web:b1ed981f9362ecca28cc2e",
+  measurementId: "G-G6LKBVXTRE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
