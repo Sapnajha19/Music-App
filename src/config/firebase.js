abb6c21d8da8bdbfd555cf3d79d98e04aa5ca892
Sapna Jha <sapnajha1919@gmail.com>
2023-11-05 16:23:38 +0530

    Initialize firebase auth

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +16,13 @@ const firebaseConfig = {
   measurementId: "G-G6LKBVXTRE"
 };
 
-// Initialize Firebase
+// // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+
+// Initialize Firebase
+// if (!getApps().length) {
+//   initializeApp(firebaseConfig);
+// }
+// Initialize Firebase auth
+export const auth = getAuth(app);
